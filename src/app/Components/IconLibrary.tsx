bf4c0be9ddcedff3229c2a791e5e5a3f6d7d83c0
Emmy Krum <ejkrum@yahoo.com>
2025-10-22 21:55:20 -0400

    fixed svg

--- src/app/Components/IconLibrary.tsx
+++ src/app/Components/IconLibrary.tsx
@@ -34,7 +34,7 @@ const iconMap: Record<string, React.FC<{size?: string; color?: string, className
     </svg>
   ),
   Library: ({ size = "1em", color = "currentColor", className="" }) => (
-    <svg className={className} stroke={color} width={size} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <svg className={className} stroke={color} width={size} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
       <path d="m16 6 4 14M12 6v14M8 8v12M4 4v16"/>
     </svg>
   ),
