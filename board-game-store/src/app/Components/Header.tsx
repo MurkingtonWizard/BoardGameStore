3119dab449df448346f86f144fbe122a356eb232
ecpre <13797480+ecpre@users.noreply.github.com>
2025-11-01 16:56:46 -0400

    add login features

--- board-game-store/src/app/Components/Header.tsx
+++ board-game-store/src/app/Components/Header.tsx
@@ -11,6 +11,25 @@ export function Header({
         search,
         filter,
         }: HeaderProps) {
+		const handleLoginPlaceholder = async () => {
+			console.log("Logging in")
+			try {
+				const response = await fetch("/login", {
+					method: "POST",
+					headers: {"Content-Type": "application/json"},
+					body: JSON.stringify({
+						email: "test@test.com",
+						password: "password"
+					}),
+					credentials: "include",
+				});
+				const data = await response.json();
+				console.log("Login response", data);
+			} catch (err) {
+				console.error("Login error", err);
+			}
+		}
+
     return (
     <header>
         <nav className="header">
@@ -18,6 +37,7 @@ export function Header({
             <Link href={"/library"} className="link"><Icon type="Library" size="2em"/></Link>
             <SearchBar search={search} filter={filter}/>
             <Link href={"/account"} className="link"><Icon type="Account" size="2em"/></Link>
+						<button type = "button" onClick = {handleLoginPlaceholder}>Login Test</button>
         </nav>
     </header>
     );
