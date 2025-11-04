592133e05e776d526ffd24ac819f4ff15e93f892
Eamonn <13797480+ecpre@users.noreply.github.com>
2025-11-04 13:14:56 -0500

    add jwt to layer

--- mysql-layer/nodejs/db/pool.mjs
+++ mysql-layer/nodejs/db/pool.mjs
@@ -1,4 +1,7 @@
 import mysql from 'mysql';
+import jwt from 'jsonwebtoken';
+
+const JWT_KEY = `${process.env.WEB_TOKEN_KEY}`;
 
 export async function createPool() {
     console.log('Creating MySQL pool...');
@@ -9,4 +12,40 @@ export async function createPool() {
         database: "bg-database"
     });
     return pool;
+}
+
+export async function generateToken(account) {
+	const payload = {
+		username: account.username,
+		email: account.email,
+		loggedIn: true	
+	};
+	
+	return new Promise(async (resolve, reject) => {
+		if (!account) reject ("account undefined");
+		return jwt.sign(payload, JWT_KEY, { expiresIn: '24h' }, (err, token) => {
+			if (err) return reject(err);
+			return resolve(token);
+		});
+	});
+		
+}
+
+export async function checkToken(token) {
+	return new Promise((resolve, reject) => {
+		if (token === undefined || token == null) return reject(new jwt.TokenExpiredError());
+
+		return jwt.verify(token, JWT_KEY, (err, decoded) => {
+			if (err) {
+				console.error(err);
+				return reject (err);
+			}
+
+			if (decoded.exp < Date.now() / 1000) {
+				return reject("expired token");
+			}
+
+			return resolve({ username: decoded.username, email: decoded.email, loggedIn: true });
+		});
+	});
 }
